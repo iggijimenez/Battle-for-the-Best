35dd1b1d1b3a1b8b955f06786b879163845db3b0
Iggi Jimenez <89427749+iggijimenez@users.noreply.github.com>
2022-03-14 13:19:10 -0700

    Initial Commit

---
+++ BallUp/BallUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallUpApp.swift
+//  BallUp
+//
+//  Created by jimenez on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BallUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
